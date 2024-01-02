2c35a41855c0b6313b7e3f24c9e018ac2c7e9999
saniaahmad6 <sania.ahmad483@gmail.com>
2024-01-03 01:40:42 +0530

    Singleton VVIP pattern

--- singleton/Singleton.cpp
+++ singleton/Singleton.cpp
@@ -1,22 +1,19 @@
-/*
- * C++ Design Patterns: Singleton
- * Author: Jakub Vojvoda [github.com/JakubVojvoda]
- * 2016
- *
- * Source code is licensed under MIT License
- * (for more details see LICENSE)
- *
- */
-
 #include <iostream>
-
+#include <thread>
+#include <mutex>
+using namespace std;
 /*
- * Singleton
- * has private static variable to hold one instance of the class
+ * Singleton - private static variable to hold one instance of the class
  * and method which gives us a way to instantiate the class
  */
 class Singleton
 {
+private:
+  static Singleton *instance;
+  static std::mutex mtx;
+  Singleton(); //defined later
+  Singleton( Singleton const& ) ;
+  Singleton& operator=( Singleton const& );
 public:
   // The copy constructor and assignment operator
   // are defined as deleted, which means that you
@@ -24,46 +21,35 @@ public:
   //
   // Note: you can achieve the same effect by declaring
   // the constructor and the operator as private
-  Singleton( Singleton const& ) = delete;
-  Singleton& operator=( Singleton const& ) = delete;
+  
 
-  static Singleton* get()
+  static Singleton* getInstance()
   {
-    if ( !instance )
+
+    if (instance==nullptr)
     {
-      instance = new Singleton();
+      mtx.lock();
+        if (instance==nullptr) {
+          return instance = new Singleton();
+        }
+      mtx.unlock();
     }    
-    return instance;
+    else return instance;
   }
   
-  static void restart()
-  {
-    if ( instance )
-    {
-      delete instance;
-    }
-  }
-  
-  void tell()
-  {
-    std::cout << "This is Singleton." << std::endl;
-    // ...
-  }
-  // ...
-
-private:
-  Singleton() {}
-  static Singleton *instance;
-  // ...
 };
 
 Singleton* Singleton::instance = nullptr;
+mutex Singleton::mtx;
 
-
+Singleton::Singleton(){
+  std::cout<<"instance"<<endl;
+}
 int main()
 {
-  Singleton::get()->tell();
-  Singleton::restart();
+  Singleton * instance=Singleton::getInstance(); //Singleton()
+  Singleton * instance1=Singleton::getInstance(); //instance returned
+  Singleton * instance2=Singleton::getInstance(); //instance returned
   
   return 0;
 }
